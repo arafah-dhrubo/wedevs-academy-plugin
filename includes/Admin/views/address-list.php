5f56c56fe60c810d0debbe47d2fc76e3cafccf29
Samiul Arafah Dhrubo <66543604+Dhrubo-Arafah@users.noreply.github.com>
2022-04-04 10:09:47 +0600

    Add: Address List Table

--- includes/Admin/views/address-list.php
+++ includes/Admin/views/address-list.php
@@ -5,4 +5,12 @@
     <a href="<?php echo admin_url('admin.php?page=wedevs-academy&action=new')?>" class="page-title-action">
         <?php _e('Add New', 'wedevs-academy')?>
     </a>
+
+    <form action="" method="post">
+        <?php
+        $table = new \WeDevs\Academy\Admin\Address_list();
+        $table->prepare_items();
+        $table->display();
+        ?>
+    </form>
 </div>
--- includes/function.php
+++ includes/function.php
@@ -5,27 +5,65 @@
  * @param $args
  * @return int|WP_Error
  */
- function wd_ac_insert_address($args=[]){
-     global $wpdb;
+function wd_ac_insert_address($args = [])
+{
+    global $wpdb;
 
-     $defaults=[
-         'name'=>$args['name'],
-         'address'=>$args['address'],
-         'phone'=>$args['phone'],
-         'email'=>$args['email'],
-         'created_by'=>get_current_user_id(),
-         'created_at'=>current_time('mysql')
-     ];
+    $defaults = [
+        'name' => $args['name'],
+        'address' => $args['address'],
+        'phone' => $args['phone'],
+        'email' => $args['email'],
+        'created_by' => get_current_user_id(),
+        'created_at' => current_time('mysql')
+    ];
 
-     $format = ['%s','%s','%s','%s','%d','%s'];
+    $format = ['%s', '%s', '%s', '%s', '%d', '%s'];
 
-     $data = wp_parse_args($args, $defaults);
+    $data = wp_parse_args($args, $defaults);
 
-     $inserted = $wpdb->insert("{$wpdb->prefix}ac_addresses", $data, $format);
+    $inserted = $wpdb->insert("{$wpdb->prefix}ac_addresses", $data, $format);
 
-     if(!$inserted){
-         return new \WP_Error('Failed to insert data', 'wedevs-academy');
-     }
+    if (!$inserted) {
+        return new \WP_Error('Failed to insert data', 'wedevs-academy');
+    }
 
-     return $wpdb->insert_id;
- }
+    return $wpdb->insert_id;
+}
+
+/**
+ * Fetching data from database
+ */
+function wd_ac_get_addresses($args = [])
+{
+    global $wpdb;
+
+    $defaults = [
+        'number' => 20,
+        'offset' => 0,
+        'order_by' => 'id',
+        'order' => 'ASC'
+    ];
+
+    $args = wp_parse_args($args, $defaults);
+
+    $sql = $wpdb->prepare(
+            "SELECT * FROM {$wpdb->prefix}ac_addresses
+                ORDER BY {$args['order_by']} {$args['order']}
+                LIMIT %d, %d"
+           ,$args['offset'],$args['number']
+    );
+
+    $items = $wpdb->get_results($sql);
+
+    return $items;
+}
+
+/**
+ * Get the count of row
+ * @return int
+ */
+function wd_ac_address_count(){
+    global $wpdb;
+    return (int) $wpdb->get_var("SELECT count(id) FROM {$wpdb->prefix}ac_addresses");
+}
